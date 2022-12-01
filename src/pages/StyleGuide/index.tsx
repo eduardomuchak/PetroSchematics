df375754355d2748e2e607ef9fb42ebf0f7abf91
Eduardo Muchak <eduardomuchak@gmail.com>
2022-12-01 13:46:58 -0300

    style: :art: adiciona icon buttons com botao editar e deletar style guide

--- src/pages/StyleGuide/index.tsx
+++ src/pages/StyleGuide/index.tsx
@@ -1,25 +1,32 @@
-import { Button, Flex, Heading, Input } from '@chakra-ui/react';
+import { FiTrash } from 'react-icons/fi';
+import { MdModeEdit } from 'react-icons/md';
 
-import ColorModeToggle from 'components/ColorModeToggle';
+import { Button, Flex, Heading, IconButton, Input } from '@chakra-ui/react';
+
+// import ColorModeToggle from 'components/ColorModeToggle';
 
 export function StyleGuide() {
   return (
     <Flex p={5} align={'center'} justify={'center'} flex={1} gap={5} direction={'column'} minH={'100vh'}>
-      <Flex align={'center'} justify={'end'}>
+      {/* <Flex align={'center'} justify={'end'}>
         <ColorModeToggle />
-      </Flex>
+      </Flex> */}
       <Flex direction={'column'} align={'center'} justify={'center'} gap={5}>
         <Heading>Botões</Heading>
         <Flex gap={5}>
-          <Flex direction={'column'} gap={5}>
+          <Flex direction={'column'} gap={5} align={'center'}>
             <Button variant={'origemBlueSolid'}>Solid</Button>
             <Button variant={'origemBlueOutline'}>Outline</Button>
             <Button variant={'origemBlueGhost'}>Ghost</Button>
+            <IconButton aria-label="Botão de Editar" icon={<MdModeEdit />} variant="origemEditSolid" />
+            <IconButton aria-label="Botão de Editar" icon={<MdModeEdit />} variant="origemEditOutline" />
           </Flex>
-          <Flex direction={'column'} gap={5}>
+          <Flex direction={'column'} gap={5} align={'center'}>
             <Button variant={'origemRedSolid'}>Solid</Button>
             <Button variant={'origemRedOutline'}>Outline</Button>
             <Button variant={'origemRedGhost'}>Ghost</Button>
+            <IconButton aria-label="Botão de Lixeira" icon={<FiTrash />} variant="origemDeleteSolid" />
+            <IconButton aria-label="Botão de Lixeira" icon={<FiTrash />} variant="origemDeleteOutline" />
           </Flex>
         </Flex>
       </Flex>
