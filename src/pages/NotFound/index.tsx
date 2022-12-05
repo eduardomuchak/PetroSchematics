e4b30a7c8bdafec16725b17a9c05f863880ad393
Eduardo Muchak <eduardomuchak@gmail.com>
2022-12-05 18:56:12 -0300

    fix: :art: ajuste responsividade pagina de not found

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -8,10 +8,10 @@ export function NotFound() {
   return (
     <Flex align="center" justify="center" height="100vh" direction={'column'}>
       <Box textAlign="center" py={10} px={6} gap={12}>
-        <Image src={NotFoundSVG} alt="Página não encontrada" w={'70vw'} />
+        <Image src={NotFoundSVG} alt="Página não encontrada" w={{ base: '100%', sm: '75vw' }} />
       </Box>
-      <Flex direction={'column'} align={'center'}>
-        <Text fontSize="18px" mb={4}>
+      <Flex direction={'column'} align={'center'} px={10}>
+        <Text fontSize="18px" mb={4} textAlign={'center'}>
           Opa! Parece que a página que você buscou não foi encontrada
         </Text>
         <Button
