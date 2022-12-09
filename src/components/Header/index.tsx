797aa8cf410595c3217dc885ab442217c8325841
Eduardo Muchak <eduardomuchak@gmail.com>
2022-12-09 18:24:05 -0300

    feat: :sparkles: adiciona botao voltar no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import { Box, Flex, Image } from '@chakra-ui/react';
 import logoImage from 'assets/logo_origem_branco.svg';
 
+import BotaoSetaVoltarHeader from 'components/BotaoSetaVoltar/BotaoSetaVoltarHeader';
+
 import NavIconBar from './NavIconBar';
 
 function Header({ children }: { children: React.ReactNode }) {
@@ -19,12 +21,13 @@ function Header({ children }: { children: React.ReactNode }) {
         px={'88px'}
         py={'22px'}
       >
-        <Flex alignItems="center" justifyContent="space-between">
+        <Flex direction={'column'} justify={'space-between'} flex={1}>
           <Link to="/">
             <Image src={logoImage} alt="Logo Origem Energias" />
           </Link>
+          <BotaoSetaVoltarHeader />
         </Flex>
-        <Box mt={'95px'} p={4} flex={1}>
+        <Box mt={'15px'} p={4} flex={1} w={'93.6vw'}>
           {children}
         </Box>
       </Box>
