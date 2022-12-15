5c03e0d682008a9facc5b45fb07da266fea45078
Bruno Alcantara <bruno.alcantara@iksolution.com.br>
2022-12-15 10:11:58 -0300

    feat: "fade formulários"

--- src/pages/Formulario/index.tsx
+++ src/pages/Formulario/index.tsx
@@ -3,7 +3,7 @@ import { AiOutlineClockCircle } from 'react-icons/ai';
 import { BsCalendarEvent } from 'react-icons/bs';
 import { useLocation } from 'react-router';
 
-import { Flex, Text } from '@chakra-ui/react';
+import { Flex, Text, Fade } from '@chakra-ui/react';
 import { aproveForm } from 'features/aprovacao';
 
 import { keyName } from 'pages/Aprovacao/keyNamePairs';
@@ -20,6 +20,7 @@ export function Formulariopage() {
   const { state }: any = useLocation();
   const [renderList, setRenderList] = useState<any[]>([]);
   const [currentIndex, setCurrentIndex] = useState<number>(state.index);
+  const [isOpen, setIsOpen] = useState(true);
 
   useEffect(() => {
     const first = state.list[state.index];
@@ -58,41 +59,45 @@ export function Formulariopage() {
   }, []);
 
   const handlePrev = (value: boolean) => {
-    const newIndex = value ? currentIndex - 1 : currentIndex + 1;
-    const first = state.list[newIndex];
-    const renderArray = [];
-    for (const property in first.form_data) {
-      if (property == 'poco') {
-        const newItem = {
-          name: 'Poco',
-          value: first.form_data[property].nome_poco,
-          type: 'poco',
-        };
-        if (typeof newItem.value === 'string' || typeof newItem.value === 'number') {
-          renderArray.splice(0, 0, newItem);
-        }
-      } else if (property == 'tanque') {
-        const newItem = {
-          name: 'Tanque',
-          value: first.form_data[property].nom_tanque,
-          type: 'poco',
-        };
-        if (typeof newItem.value === 'string' || typeof newItem.value === 'number') {
-          renderArray.splice(0, 0, newItem);
-        }
-      } else {
-        const newItem = {
-          name: keyName.filter((val: any) => val.key === property)[0].title,
-          value: first.form_data[property],
-          type: keyName.filter((val: any) => val.key === property)[0].type,
-        };
-        if (typeof newItem.value === 'string' || typeof newItem.value === 'number') {
-          renderArray.push(newItem);
+    setIsOpen(false);
+    setTimeout(() => {
+      const newIndex = value ? currentIndex - 1 : currentIndex + 1;
+      const first = state.list[newIndex];
+      const renderArray = [];
+      for (const property in first.form_data) {
+        if (property == 'poco') {
+          const newItem = {
+            name: 'Poco',
+            value: first.form_data[property].nome_poco,
+            type: 'poco',
+          };
+          if (typeof newItem.value === 'string' || typeof newItem.value === 'number') {
+            renderArray.splice(0, 0, newItem);
+          }
+        } else if (property == 'tanque') {
+          const newItem = {
+            name: 'Tanque',
+            value: first.form_data[property].nom_tanque,
+            type: 'poco',
+          };
+          if (typeof newItem.value === 'string' || typeof newItem.value === 'number') {
+            renderArray.splice(0, 0, newItem);
+          }
+        } else {
+          const newItem = {
+            name: keyName.filter((val: any) => val.key === property)[0].title,
+            value: first.form_data[property],
+            type: keyName.filter((val: any) => val.key === property)[0].type,
+          };
+          if (typeof newItem.value === 'string' || typeof newItem.value === 'number') {
+            renderArray.push(newItem);
+          }
         }
       }
-    }
-    setCurrentIndex(newIndex);
-    setRenderList(renderArray);
+      setCurrentIndex(newIndex);
+      setRenderList(renderArray);
+      setIsOpen(true);
+    }, 1000);
   };
 
   const handleModal = async (aproved: boolean) => {
@@ -110,43 +115,45 @@ export function Formulariopage() {
       {/* <TituloPagina botaoVoltar>Formulário para Aprovação</TituloPagina> */}
       <Flex justify={'center'} gap={10} direction={'row'} flex={1}>
         {currentIndex == 0 ? undefined : <ModalPrevious handle={handlePrev} />}
-        <Flex direction={'column'}>
-          <Flex align={'center'} gap={4} w={'700px'} flexWrap="wrap">
-            {renderList.map((item: any) => (
-              <Flex flexGrow={1} minW={item.type == 'multiline' ? '500px' : '200px'} direction={'column'}>
-                <Text fontWeight={700} fontSize={12} letterSpacing={0.3} color={'#949494'}>
-                  {item.name}
-                </Text>
-                {item.type == 'poco' ? (
-                  <Text fontWeight={700} fontSize={20} letterSpacing={0.3} color={'#0048BB'}>
-                    {item.value}
+        <Fade in={isOpen}>
+          <Flex direction={'column'}>
+            <Flex align={'center'} gap={4} w={'700px'} flexWrap="wrap">
+              {renderList.map((item: any) => (
+                <Flex flexGrow={1} minW={item.type == 'multiline' ? '500px' : '200px'} direction={'column'}>
+                  <Text fontWeight={700} fontSize={12} letterSpacing={0.3} color={'#949494'}>
+                    {item.name}
                   </Text>
-                ) : (
-                  <Flex
-                    borderWidth={'1px'}
-                    borderColor={'#949494'}
-                    borderRadius={'8px'}
-                    h={item.type == 'multiline' ? '200px' : '56px'}
-                    align={item.type == 'multiline' ? undefined : 'center'}
-                    p={'8px'}
-                    justify={'space-between'}
-                  >
-                    <Text fontWeight={400} fontSize={12} letterSpacing={0.3} color={'#949494'}>
+                  {item.type == 'poco' ? (
+                    <Text fontWeight={700} fontSize={20} letterSpacing={0.3} color={'#0048BB'}>
                       {item.value}
                     </Text>
-                    {item.type == 'time' ? <AiOutlineClockCircle size={24} /> : undefined}
-                    {item.type == 'date' ? <BsCalendarEvent size={22} /> : undefined}
-                  </Flex>
-                )}
-              </Flex>
-            ))}
+                  ) : (
+                    <Flex
+                      borderWidth={'1px'}
+                      borderColor={'#949494'}
+                      borderRadius={'8px'}
+                      h={item.type == 'multiline' ? '200px' : '56px'}
+                      align={item.type == 'multiline' ? undefined : 'center'}
+                      p={'8px'}
+                      justify={'space-between'}
+                    >
+                      <Text fontWeight={400} fontSize={12} letterSpacing={0.3} color={'#949494'}>
+                        {item.value}
+                      </Text>
+                      {item.type == 'time' ? <AiOutlineClockCircle size={24} /> : undefined}
+                      {item.type == 'date' ? <BsCalendarEvent size={22} /> : undefined}
+                    </Flex>
+                  )}
+                </Flex>
+              ))}
+            </Flex>
+            <Flex justify={'center'} gap={2} mt={'24px'} direction={'row'} w={'100%'}>
+              <ModalReprove handle={handleModal} />
+              <ModalAprove handle={handleModal} />
+            </Flex>
           </Flex>
-          <Flex justify={'center'} gap={2} mt={'24px'} direction={'row'} w={'100%'}>
-            <ModalReprove handle={handleModal} />
-            <ModalAprove handle={handleModal} />
-          </Flex>
-        </Flex>
-        {currentIndex == state.list.length - 1 ? undefined : <ModalNext handle={handlePrev} />}
+        </Fade>
+        <Flex mr={50}>{currentIndex == state.list.length - 1 ? undefined : <ModalNext handle={handlePrev} />}</Flex>
       </Flex>
     </GridLayout>
   );
