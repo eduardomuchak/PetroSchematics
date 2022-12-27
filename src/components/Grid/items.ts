aa2622eb7a7e55a7418c5c5bbad816e86153037c
Bruno Alcantara <bruno.alcantara@iksolution.com.br>
2022-12-27 13:55:07 -0300

    fix: "nome da tela"

--- src/components/Grid/items.ts
+++ src/components/Grid/items.ts
@@ -4,6 +4,6 @@ import { TbListSearch } from 'react-icons/tb';
 
 export const sidebarIcons = [
   { name: 'Lista de Poços', icon: TbListSearch, link: '/lista-pocos' },
-  { name: 'Lista de Aprovações', icon: BsFileCheck, link: '/aprovacao' },
+  { name: 'Módulo de Aprovações', icon: BsFileCheck, link: '/aprovacao' },
   // { name: 'Style Guide', icon: BsPalette, link: '/style-guide' },
 ];
