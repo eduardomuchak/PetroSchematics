65a60c8b7f14ba80c249862bd6c7b569c585128c
Eduardo Muchak <eduardomuchak@gmail.com>
2023-01-02 14:27:07 -0300

    feat: :sparkles: construcao date time picker em andamento

--- src/components/DateTimePicker/index.tsx
+++ src/components/DateTimePicker/index.tsx
@@ -1,17 +1,28 @@
 import { forwardRef, useState } from 'react';
-import ReactDatePicker from 'react-datepicker';
+import ReactDatePicker, { registerLocale } from 'react-datepicker';
 
-import { Button, Flex, FormControl, Text } from '@chakra-ui/react';
+import { Button, Flex, Text } from '@chakra-ui/react';
+// import { getDay } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 import { RequiredField } from 'components/RequiredField/RequiredField';
+registerLocale('pt-BR', ptBR);
 
 interface Props {
   label: string;
   isRequired?: boolean;
+  showTimeSelect?: boolean;
+  isDisabled?: boolean;
+  dateFormat: 'Pp' | 'dd/MM/yyyy';
 }
 
-function DateTimePicker({ label, isRequired }: Props) {
-  const [date, setDate] = useState<any>('');
+function DateTimePicker({ label, isRequired, showTimeSelect, isDisabled, dateFormat }: Props) {
+  const [date, setDate] = useState<Date | null>(null);
+
+  // const isWeekday = (date: Date) => {
+  //   const day = getDay(date);
+  //   return day !== 0 && day !== 6;
+  // };
 
   const Trigger = forwardRef(({ value, onClick }: any, ref: any) => (
     <Button
@@ -39,8 +50,12 @@ function DateTimePicker({ label, isRequired }: Props) {
     </Button>
   ));
 
+  // if (date) {
+  //   console.log('date', date.toString());
+  // }
+
   return (
-    <FormControl>
+    <Flex direction={'column'}>
       <Flex gap={1}>
         {isRequired ? <RequiredField /> : null}
         <Text fontWeight={'700'} fontSize={'12px'} color={'#949494'}>
@@ -48,12 +63,16 @@ function DateTimePicker({ label, isRequired }: Props) {
         </Text>
       </Flex>
       <ReactDatePicker
+        // filterDate={isWeekday}
+        disabled={isDisabled}
+        showTimeSelect={showTimeSelect}
+        locale="pt-BR"
         selected={date}
         onChange={(date: any) => setDate(date)}
-        dateFormat="dd/MM/yyyy"
+        dateFormat={dateFormat}
         customInput={<Trigger />}
       />
-    </FormControl>
+    </Flex>
   );
 }
 
--- src/pages/StyleGuide/index.tsx
+++ src/pages/StyleGuide/index.tsx
@@ -1,5 +1,3 @@
-import { forwardRef, useState } from 'react';
-import ReactDatePicker from 'react-datepicker';
 import { FiTrash } from 'react-icons/fi';
 import { MdModeEdit } from 'react-icons/md';
 
@@ -18,45 +16,18 @@ import {
   Text,
 } from '@chakra-ui/react';
 
+import DateTimePicker from 'components/DateTimePicker';
 import GridLayout from 'components/Grid';
 import { RequiredField } from 'components/RequiredField/RequiredField';
 import SelectFiltragem from 'components/SelectFiltragem';
 
 export function StyleGuide() {
-  const [date, setDate] = useState<any>('');
-
   const options = [
     {
       value: 1,
       label: 'Opção 1',
     },
   ];
-
-  const TriggerDatePickerInicio = forwardRef(({ value, onClick }: any, ref: any) => (
-    <Button
-      h={'56px'}
-      w={'100%'}
-      onClick={onClick}
-      ref={ref}
-      variant="outline"
-      px={5}
-      minW={'220px'}
-      borderRadius={'8px'}
-      color={value === '' ? '#575757' : '#000'}
-      _focus={{
-        bg: 'white',
-        borderColor: 'origem.300',
-        borderWidth: '2px',
-      }}
-      _hover={{
-        bg: 'white',
-        borderColor: 'hsl(0, 0%, 70%)',
-        borderWidth: '1px',
-      }}
-    >
-      {value === '' ? 'Selecione uma data' : value}
-    </Button>
-  ));
 
   return (
     <GridLayout>
@@ -115,17 +86,8 @@ export function StyleGuide() {
             value={options[0]}
             dispatchAction={() => {}}
           />
-          <Flex direction={'column'}>
-            <Text fontWeight={'700'} fontSize={'12px'} color={'#949494'}>
-              DATE PICKER
-            </Text>
-            <ReactDatePicker
-              selected={date}
-              onChange={(date: any) => setDate(date)}
-              dateFormat="dd/MM/yyyy"
-              customInput={<TriggerDatePickerInicio />}
-            />
-          </Flex>
+          <DateTimePicker label={'DATETIME PICKER'} showTimeSelect={true} dateFormat={'Pp'} />
+          <DateTimePicker label={'DATE PICKER'} isRequired={true} dateFormat={'dd/MM/yyyy'} />
         </Flex>
       </Flex>
     </GridLayout>
